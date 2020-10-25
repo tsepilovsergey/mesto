3fc58c8ef09eba8036197c3c5773f67bd4a98132
tsepilovsergey <tsepilovsergey@yandex.kz>
2020-10-25 18:14:54 +0600

    Sprint 5: Project Work 4! Index JavaScript

---
+++ script/index.js
@@ -0,0 +1,30 @@
+let content = document.querySelector('.content');
+let profileEditButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let popupCloseButton = document.querySelector('.popup__close-button');
+let formElement = document.querySelector('.popup__form');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_description');
+
+function openPopup () {
+    popup.classList.add('popup_opened');
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
+}
+
+function closePopup () {
+    popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+    closePopup ();
+}
+
+profileEditButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
